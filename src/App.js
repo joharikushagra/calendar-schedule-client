48b9ad3b21c53cdfadd484745a7b2f18ebb16a37
joharikushagra <skjkushagra1309@gmail.com>
2023-04-06 01:54:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "./App.css";
+import 'react-calendar/dist/Calendar.css';
+
+
+export default function App() {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} tileContent="hjjgf"/>
+    </div>
+  );
+}
